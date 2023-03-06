2cde658e6b034c99c9e3f9538d7516d32b140e13
adam leach <100628495+MisterBooty@users.noreply.github.com>
2023-03-07 12:19:46 +1300

    Tuesday

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 EXAMPLE README 
 
 02/03 Came up with a idea for my parameterisedSpace project. It consists of a wallpaper of cubes, which has waves flowing through it. These waves are represented by certain faces of a row of cubes extending and then retreating like a 3d bar graph. These waves will be skipping rows in order for them to bee seen more easily. once a wave has passed, there will be a point in which all cubes are netural sizez, before extending again. However, with each extension and retreat the cubes will then change what face they extend on, giving the wave a more 3d effect.
+
+07/03 messed around more with the expanding cube. After watching todays class, i believe that there are a lot of things that i can take into my away and implemnt into my own code. I believe that there are some new strategies that i could use to achieve my goal.
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,50 +1,41 @@
-var x=300;
-var y=300;
-var a=100;
-var b=100;
-
-function draw_one_frame() {
-  //background(255);
-  x+=2;
-  y+=2;
-	a-=2;
-	b-=2;
-	strokeWeight(1);
-  translate(width/2, height/2);
-  for(var i=0;i<15;i++){
-	  for(var k=0;k<20;k++){
-		stroke(255,255,255);
-    rotate(PI / 12.0);
-	  fill(255,255-i*10,255-k*10);
-  	line(a%100,b%100,x%300,y%300);
-	  ellipse((x+i*20)%width,(y+k*20)%height,i+4,i+4);
-		drawtriangle((a-i*20)%width,(b-k*20)%height,k/8);
-		rect(x%width, y%height, k+10, k+10);
-		fill(0,i*10,255-k*10);
-		ellipse((x-i*20)%width,(y-k*20)%height,i+3,i+3);
-		rotate(PI / 24.0);
-		fill(255-(i+k)*5,(i+k)*7,i*20);
-		drawtriangle((a+i*20)%width,(b+k*20)%height,k/8);
-		rect(a%width, b%height, k+5, k+5);
-		drawflower(k,x);
-	  }
-  }
-
-}
-
-function drawtriangle(x,y,r){
-	triangle(x, y, x+7*r, y-13.75*r, x+14*r, y);
-}
-
-function drawflower(i,k){
-		if(i%2==1){
-			fill(255,(k*0.4)%255,30);
-			stroke(k%255,255,0);
-			arc(0,0,150+i+150*sin(k*PI/24),150,0,PI / 40);
+function draw_one_frame(cur_frac) {
+	noStroke()
+	let backgroundColor = color("#dbbea1")
+	fill(backgroundColor)
+	rect(0,0, width, height)
+	////////////////////////////////////////////
+	let mainColor = color("#3F292B") // brown
+	let backupColor = color("#D34F73") // blush
+	let DetailColor = color("#DB7F67")
+	
+	
+	let noiseColor;
+	let noiseyColor; 
+	let moveXMap;
+	
+	
+	let orbSize = width / 20
+	let spacingSize = width / 19
+	
+	//////////////////////////////////////////////
+	fill(mainColor)
+	
+	for(let accross = 1; accross < width /spacingSize; accross++ ){
+		for(let down = 1; down +1 < height /spacingSize; down++){		
+			
+		noiseColor = getNoiseValue(spacingSize*accross,spacingSize*down, 0.8, "noiseColor",0,1, 200 )
+		noiseyLerp = lerpColor(mainColor,backupColor,noiseColor)  // https://p5js.org/reference/#/p5/lerpColor
+		fill(noiseyLerp)
+		ellipse(spacingSize*accross,spacingSize*down ,orbSize)
+	
+			if(cur_frac > 0.3 && noiseColor < 0.3){
+				fill(225)
+				moveXMap = map(cur_frac,0.3, 1, spacingSize*accross, spacingSize*(accross+1))
+				ellipse(moveXMap,spacingSize*down,orbSize/2) 
+			}
 		}
-		else{
-			fill(k%255,0,255);
-			stroke(0,(k*0.4)%255,255);
-			arc(0,0,(100+100*cos(k*PI/24))%255,50,0,PI / 20);
-		}
-}
+	}
+	
+	}
+	
+	
