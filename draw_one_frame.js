fbdddc1703d78b089281193e555f8e871f4d92f2
adam leach <100628495+MisterBooty@users.noreply.github.com>
2023-03-07 13:58:00 +1300

    see readme

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,40 +1,75 @@
 function draw_one_frame(cur_frac) {
 	noStroke()
-	let backgroundColor = color("#dbbea1")
+	let backgroundColor = color("#4C061D") // burnt red
 	fill(backgroundColor)
 	rect(0,0, width, height)
 	////////////////////////////////////////////
-	let mainColor = color("#3F292B") // brown
-	let backupColor = color("#D34F73") // blush
-	let DetailColor = color("#DB7F67")
+	let TopColor = color("#2F4858");// dark blue
+	let Leftcolor = color("#F6AE2D"); // mustard yellow
+	let Rightcolor = color("#F26419"); // ORANGE ORANGE
+
+	
+	let noiseyColor;
+	///////////////////////////////////////////
+
+	let rectSize = width / 12.01
+	let SpaceSize = width /6
+	
+	///////////////////////////////////////////
+
+	fill(Leftcolor)
+
+for( let across = 0; across < width / SpaceSize; across++){
+	for( let down = 0; down < height / SpaceSize; down++){
+
+		let noiseColor = getNoiseValue(SpaceSize*across,SpaceSize*down,0.8, "noiseColor",0,3)
+		rect(SpaceSize*across,SpaceSize*down-30, rectSize, rectSize)
+		rect(SpaceSize*across + 80,SpaceSize*down+40, rectSize, rectSize)
+		
+		
+
+	}
+}
+	
+fill(Rightcolor);
+for( let across= 0; across < width / SpaceSize; across++){
+	for( let down = -1.1; down < height / SpaceSize; down++){
+
+		let noiseColor = getNoiseValue(SpaceSize*across,SpaceSize*down,0.8, "noiseColor",0,3)
+		rect(SpaceSize*across,SpaceSize*down-90, rectSize, rectSize)
+		rect(SpaceSize*across + 80,SpaceSize*down-10, rectSize, rectSize)
+		
+		
+
+	}
+}
+
+//	let noiseColor;
+//	let noiseyColor; 
+//	let moveXMap;
 	
 	
-	let noiseColor;
-	let noiseyColor; 
-	let moveXMap;
-	
-	
-	let orbSize = width / 20
-	let spacingSize = width / 19
+	//let rectSize = width / 20
+	//let spacingSize = width/20
 	
 	//////////////////////////////////////////////
-	fill(mainColor)
+//	fill(TopColor)
 	
-	for(let accross = 1; accross < width /spacingSize; accross++ ){
-		for(let down = 1; down +1 < height /spacingSize; down++){		
+	//for(let across = 1; across < width /spacingSize; across++ ){
+//		for(let down = 1; down +1 < height /spacingSize; down++){		
 			
-		noiseColor = getNoiseValue(spacingSize*accross,spacingSize*down, 0.8, "noiseColor",0,1, 200 )
-		noiseyLerp = lerpColor(mainColor,backupColor,noiseColor)  // https://p5js.org/reference/#/p5/lerpColor
-		fill(noiseyLerp)
-		ellipse(spacingSize*accross,spacingSize*down ,orbSize)
+//		noiseColor = getNoiseValue(spacingSize*across,spacingSize*down, 0.8, "noiseColor",0,1, 200 )
+//		noiseyLerp = lerpColor(TopColor, Leftcolor,Rightcolor)  // https://p5js.org/reference/#/p5/lerpColor
+//		fill(noiseyLerp)
+//		rect(spacingSize*across,spacingSize*down ,rectSize)
 	
-			if(cur_frac > 0.3 && noiseColor < 0.3){
-				fill(225)
-				moveXMap = map(cur_frac,0.3, 1, spacingSize*accross, spacingSize*(accross+1))
-				ellipse(moveXMap,spacingSize*down,orbSize/2) 
-			}
-		}
-	}
+//			if(cur_frac > 0.3 && noiseColor < 0.3){
+//				fill(225)
+//				moveXMap = map(cur_frac,0.3, 1, spacingSize*across, spacingSize*(across+1))
+//				rect(moveXMap,spacingSize*down,rectSize/2) 
+//			}
+//		}
+//	}
 	
 	}
 	
